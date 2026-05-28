2fbe7f5fc4ce3832a8bc47504678e4cf57e864c9
alakambhavya <alakambhavya@gmail.com>
2026-05-28 08:33:37 +0530

    Update identity_op.py
    changes in identity OP

--- identity_op.py
+++ identity_op.py
@@ -1,6 +1,8 @@
 #identity operator
 data="bhavya"
 data_1=data
+print("data id : ",id(data))
+print("data_1 id : ",id(data_1))
 print(data_1 is data)
 print(data_1 is "std")
 print(data_1 is "data")
@@ -9,5 +11,7 @@ print("____________")
 
 name="bhanu"
 data=name
+print("name id : ",id(name))
+print("data id : ",id(data))
 print(data is not name)
 print(data is not "name")
